f08205de9ee7d71520c995fe2736f8a5cc678449
iagokrt <iago_floripa@hotmail.com>
2020-01-17 17:09:01 -0300

    physics arcade< added

--- game.js
+++ game.js
@@ -2,7 +2,14 @@ var config = {
   width: 450,
   height: 162,
   backgroundColor: "000000  ",
-  scene: [Scene1, Scene2]
+  scene: [Scene1, Scene2],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: false
+    }
+  }
 };
 
 window.onload = function() {
